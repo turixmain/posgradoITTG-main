5f6f6a455095ade738275278a84fea6133f3a5a5
turixmain <control4everyone@gmail.com>
2026-05-19 12:45:28 -0600

    Preparando deploy render 2

--- mcim_dciittg/settings.py
+++ mcim_dciittg/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
